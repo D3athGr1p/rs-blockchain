4c22b6ee18bd4a9f13e340009664e27be8d6fca8
Kishan Sudani <86771928+D3athGr1p@users.noreply.github.com>
2024-05-25 17:41:54 +0530

    Implementing the threading
    threads for Transaction listing and creating block
    
    solving issue  in create_transaction

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,63 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+
 mod block;
 mod blockchain;
 mod transaction;
 
-use std::{thread::sleep, time::Duration};
+use rocket::serde::json::{json, Value};
+use rocket::State;
+use rocket_contrib::json::Json;
+
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
 
 use blockchain::Blockchain;
 
 const BLOCKTIME: u64 = 1; // 1 second
-const MAX_COUNT: usize = 3;
+
+#[post("/transaction", data = "<tx>")]
+fn create_transaction(
+    blockchain: &State<Arc<Mutex<Blockchain>>>,
+    tx: Json<transaction::Transaction>,
+) -> Value {
+    let mut blockchain = blockchain.lock().unwrap();
+    blockchain.create_transaction(tx.sender.clone(), tx.recipient.clone(), tx.amount);
+    json!({"message": "Transaction added to pending transactions"})
+}
 
 fn main() {
-    let mut blockchain = Blockchain::new();
+    let blockchain = Arc::new(Mutex::new(Blockchain::new()));
 
-    let mut amount = 1000;
-    let mut count = 0;
+    rocket::build()
+        .manage(blockchain.clone())
+        .mount("/", routes![create_transaction])
+        .launch();
 
-    let (mut sender, mut recipient) = ("alice".to_owned(), "bob".to_owned());
-    while count < MAX_COUNT {
-        blockchain.create_transaction(sender.clone(), recipient.clone(), amount);
-        let block = blockchain.create_new_block();
-        if !block.validate(blockchain.difficulty).unwrap() {
-            eprintln!("Invalid Block found");
-            let block = blockchain.chain.pop();
-            blockchain.pending_transactions = block.unwrap().transactions;
+    let create_block = Arc::clone(&blockchain);
+
+    let create_block_handle = thread::spawn(move || {
+        let mut blockchain = create_block.lock().unwrap();
+
+        loop {
+            let block = blockchain.create_new_block();
+
+            if !block.validate(blockchain.difficulty).unwrap() {
+                eprintln!("Invalid Block found");
+                let block = blockchain.chain.pop();
+                blockchain.pending_transactions = block.unwrap().transactions;
+            }
+            blockchain.chain.last().unwrap().print_block_json();
+            thread::sleep(Duration::from_secs(BLOCKTIME));
         }
+    });
 
-        (sender, recipient) = (recipient, sender);
-        amount /= 2;
-
-        sleep(Duration::from_secs(BLOCKTIME));
-        count += 1;
-    }
-
-    blockchain.chain.last().unwrap().print_block_json();
+    create_block_handle.join().unwrap();
 }
